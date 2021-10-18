07c22334fb3a619ca1d06488f28f9152826007e4
Jonathan Kelley <jkelleyrtp@gmail.com>
2021-10-18 18:23:24 -0400

    feat: finish

--- src/lib.rs
+++ src/lib.rs
@@ -2,21 +2,6 @@ use dioxus::core::*;
 use dioxus::prelude::*;
 use pulldown_cmark::Parser;
 
-pub fn render_markdown<'a>(cx: Context<'a>, text: &str) -> DomTree<'a> {
-    let parser = Parser::new(text);
-
-    let mut html_buf = String::new();
-    pulldown_cmark::html::push_html(&mut html_buf, parser);
-
-    cx.render(rsx!(div {
-        dangerous_inner_html: format_args!("{}", html_buf),
-    }))
-}
-
-pub struct Markdown<'a> {
-    contents: &'a str,
-}
-
 #[derive(PartialEq, Props)]
 pub struct StaticMarkdownProps {
     contents: String,
@@ -44,3 +29,14 @@ pub struct StaticMarkdownProps {
 /// ```
 pub static StaticMarkdown: FC<StaticMarkdownProps> =
     |(cx, props)| render_markdown(cx, &props.contents);
+
+pub fn render_markdown<'a>(cx: Context<'a>, text: &str) -> DomTree<'a> {
+    let parser = Parser::new(text);
+
+    let mut html_buf = String::new();
+    pulldown_cmark::html::push_html(&mut html_buf, parser);
+
+    cx.render(rsx!(div {
+        dangerous_inner_html: format_args!("{}", html_buf),
+    }))
+}
