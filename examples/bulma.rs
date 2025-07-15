e431f3ab4f07abaa76f29ce9949de4d159bc2874
Daniel D’Aquino <daniel@daquino.me>
2025-07-15 12:06:21 -0700

    Render markdown to Dioxus nodes using ASTs
    This commit is a proof-of-concept which changes the strategy from
    rendering directly to HTML to rendering markdown to an abstract syntax
    tree, and then render those to Dioxus nodes.

--- examples/bulma.rs
+++ examples/bulma.rs
@@ -12,6 +12,6 @@ fn app() -> Element {
             rel: "stylesheet",
             href: "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
         }
-        div { class: "container is-fluid", Markdown { class: class, content: include_str!("../README.md") } }
+        div { class: "container is-fluid", Markdown { class: class, content: include_str!("./example.md") } }
     }
 }
